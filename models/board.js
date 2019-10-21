2d03bf2144ed315a0c2549e2a262733461ecb683
Pierre-Alexis <pierre.alexis.blond@gmail.com>
2019-10-21 15:58:51 +0200

    create models

--- models/board.js
+++ models/board.js
@@ -3,6 +3,20 @@ const mongoose = require("mongoose")
 module.exports = mongoose.model(
   "board",
   new mongoose.Schema({
-    
+    text: String,
+    board: String,
+    created_on: String,
+    bumped_on: String,
+    reported: Boolean,
+    password: String,
+    replies: [
+      {
+        reply_id: String,
+        text: String,
+        delete_password: String,
+        reported: Boolean
+      }
+    ],
+    replycount: Number
   })
 )
